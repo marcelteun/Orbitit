0ff49ab6481f0a1667d50ebc3ec4969a1728c84e
Marcel Tunnissen <marcelteun@gmail.com>
2011-05-30 21:32:12 +0200

    folded heptagons: add new solutions
    This patch adds solution files that didn't exist yet. This includes
    solutions with 8 triangles.
    
    Signed-off-by: Marcel Tunnissen <marcelteun@gmail.com>

---
+++ Data_FldHeptA4/frh-roots-0_1_0_1_0_0_1-fld_w.0-shell_1_loose-opp_alt_rot_strip_1_loose.py
@@ -0,0 +1,11 @@
+# edgeLengths = [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0]
+# edgeAlternative = shell 1 loose
+# oppEdgeAlternative = alt rot strip 1 loose
+# fold = w
+# 110530 163920: 1 solutions found
+iterations = 44000
+results = [
+    [1.55586186982417, 0.06980439471254, -3.13275304575652, 1.55252263739135, -0.19173886524000, 1.45369739681563, 1.83546751768561],
+]
+results_refl = [
+]
