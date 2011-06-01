576f55e6c965525aa1b5ea1ef8d16d9ba6ebdfe2
Marcel Tunnissen <marcelteun@gmail.com>
2011-06-01 20:14:31 +0200

    folded heptagons: 34 more solutions
    Signed-off-by: Marcel Tunnissen <marcelteun@gmail.com>

--- Data_FldHeptA4/frh-roots-0_1_0_1_0_0_1-fld_w.0-shell_1_loose-opp_alt_rot_strip_1_loose.py
+++ Data_FldHeptA4/frh-roots-0_1_0_1_0_0_1-fld_w.0-shell_1_loose-opp_alt_rot_strip_1_loose.py
@@ -2,8 +2,8 @@
 # edgeAlternative = shell 1 loose
 # oppEdgeAlternative = alt rot strip 1 loose
 # fold = w
-# 110530 163920: 1 solutions found
-iterations = 44000
+# 110601 143638: 1 solutions found
+iterations = 336000
 results = [
     [1.55586186982417, 0.06980439471254, -3.13275304575652, 1.55252263739135, -0.19173886524000, 1.45369739681563, 1.83546751768561],
 ]
