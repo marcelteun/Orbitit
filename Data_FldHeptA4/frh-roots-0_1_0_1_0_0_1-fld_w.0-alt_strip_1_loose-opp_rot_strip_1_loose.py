ec803c3dd3015f2f3a93ccd76b269920af47ae9b
Marcel Tunnissen <marcelteun@gmail.com>
2011-06-07 21:38:15 +0200

    folded heptagons: 66 more solutions (A4)
    Signed-off-by: Marcel Tunnissen <marcelteun@gmail.com>

--- Data_FldHeptA4/frh-roots-0_1_0_1_0_0_1-fld_w.0-alt_strip_1_loose-opp_rot_strip_1_loose.py
+++ Data_FldHeptA4/frh-roots-0_1_0_1_0_0_1-fld_w.0-alt_strip_1_loose-opp_rot_strip_1_loose.py
@@ -2,8 +2,8 @@
 # edgeAlternative = alt strip 1 loose
 # oppEdgeAlternative = rot strip 1 loose
 # fold = w
-# 110601 150425: 1 solutions found
-iterations = 340000
+# 110607 100247: 1 solutions found
+iterations = 1144000
 results = [
     [-1.32200223415996, -2.36423423075489, -1.75383477143902, 0.83535550988281, 0.93006082029268, -1.75383477143902, 0.83535550988281],
 ]
--- Data_FldHeptA4/frh-roots-0_1_0_1_0_0_1-fld_w.0-shell_1_loose-opp_rot_strip_1_loose.py
+++ Data_FldHeptA4/frh-roots-0_1_0_1_0_0_1-fld_w.0-shell_1_loose-opp_rot_strip_1_loose.py
@@ -2,8 +2,8 @@
 # edgeAlternative = shell 1 loose
 # oppEdgeAlternative = rot strip 1 loose
 # fold = w
-# 110601 143330: 1 solutions found
-iterations = 336000
+# 110607 100929: 1 solutions found
+iterations = 1144000
 results = [
     [1.69751398550576, -0.39526987264380, 0.44243261618783, 0.38512476607222, 1.52666008938264, -0.33441751673565, 1.20196940579261],
 ]
